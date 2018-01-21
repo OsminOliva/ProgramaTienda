404ba2e4d7ad0292bec8e7cd5f284ac6bdc3167d
Edson Andrez <edson.andrez@outlook.es>
2018-01-21 12:54:39 -0600

    Se arregló el problema del DAO padre

--- src/DAOs/DAO.java
+++ src/DAOs/DAO.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 
 public interface DAO <T,k>{
-    void insertar();
-    void actualizar();
+    void insertar(T a);
+    void actualizar(T a);
     List<T> obtenerTodo();
     T obtenerUno(k id);
     
